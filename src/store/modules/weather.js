f5e70941d1def8be4a0985a7c09cbd4ae6bfa663
Dave Regg <drregg6@gmail.com>
2019-10-16 19:58:19 -0400

    API call to openweather works

--- src/store/modules/weather.js
+++ src/store/modules/weather.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const ALT_API = process.env.VUE_APP_ALT_API;
 
 const state = {
   weather: {}
@@ -9,11 +10,19 @@ const getters = {
 };
 
 const actions = {
-  getWeather: async ({ commit }) => {
-    const res = await axios.get("https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22");
-    console.log(res.data);
-
-    // commit("commitWeather", res.data);
+  async getWeather({ commit }) {
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${ALT_API}`
+      );
+      commit("commitWeather", res.data);
+      /* eslint-disable no-console */
+      console.log(res.data);
+      /* eslint-enable no-console */
+    } catch(err) {
+      // eslint-disable-next-line
+      console.error(err);
+    }
   }
 };
 
