ab1cad09587935066a164e3189e3cf1009dc0a32
Dave Regg <drregg6@gmail.com>
2019-10-19 11:51:37 -0400

    removed insignificant helpers

--- src/helpers/celsToFahr.js
+++
@@ -1,4 +0,0 @@
-export default (temp) => {
-  temp = parseFloat(temp);
-  return `${(temp * 9/5 + 32).toFixed(1)}${String.fromCharCode(176)}F`
-}
--- src/helpers/fahrToCels.js
+++
@@ -1,4 +0,0 @@
-export default (temp) => {
-  temp = parseFloat(temp);
-  return `${((temp - 32) * 5/9).toFixed(1)}${String.fromCharCode(176)}C`
-}
